c253c164a204ab2bcaa922072d07a180fb3b106a
Touger Thao <tougerthao@gmail.com>
2022-02-11 13:31:00 -0600

    step 2 completed

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+    let API_KEY: String = String::from("api key removed");
+
+    let mut params = HashMap::new();
+    params.insert("symbol", "BTC");
+
+    let client = reqwest::Client::new();
+
+    let resp = client
+        .get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest")
+        .header("X-CMC_PRO_API_KEY", API_KEY)
+        .query(&params)
+        .send()
         .await?;
 
-    println!("{:?}", resp);
+    let resp = resp.text().await?;
+
+    println!("{:#?}", resp);
+
     Ok(())
 }
